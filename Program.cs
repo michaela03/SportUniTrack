f56910c7cb34878d9378a9bccfbac8933a6ad5cc
Michaela Kolarova <60810622+michaela03@users.noreply.github.com>
2024-03-30 21:00:16 +0200

    Modified   Program.cs Modified   SportUniTrack.csproj

--- Program.cs
+++ Program.cs
@@ -21,40 +21,41 @@ builder.Services.AddRazorPages();
 builder.Services.AddAuthorization(options =>
 {
     options.AddPolicy("RequireAdminRole",
-         policy => policy.RequireRole("Admin"));
+         policy => policy.RequireRole("adminRoleId"));
 });
 
 var app = builder.Build();
 
 //add admin role
-using (var scope = app.Services.CreateScope())
-{
-    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+//using (var scope = app.Services.CreateScope())
+//{
+//    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+//    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+//    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
-    // Apply migrations
-    dbContext.Database.Migrate();
+//    // Apply migrations
+//    dbContext.Database.Migrate();
 
-    // Seed roles
-    if (!await roleManager.RoleExistsAsync("Admin"))
-    {
-        await roleManager.CreateAsync(new IdentityRole("Admin"));
-    }
+//    //// Seed roles
+//    if (!await roleManager.RoleExistsAsync("adminRoleId"))
+//    {
+//        await roleManager.CreateAsync(new IdentityRole("adminRoleId"));
+//    }
 
-    // Seed admin user
-    var adminEmail = "mindscriptduo@gmail.com";
-    var adminExists = await userManager.FindByEmailAsync(adminEmail);
-    if (adminExists == null)
-    {
-        var adminUser = new IdentityUser { UserName = adminEmail, Email = adminEmail };
-        var result = await userManager.CreateAsync(adminUser, ":''Qh8NY9_73"); // Set the password for the admin user
-        if (result.Succeeded)
-        {
-            await userManager.AddToRoleAsync(adminUser, "Admin");
-        }
-    }
-}
+//    // Seed admin user
+//    var adminEmail = "admin@gmail.com";
+//    var adminExists = await userManager.FindByEmailAsync(adminEmail);
+//    if (adminExists == null)
+//    {
+//        var adminUser = new IdentityUser { UserName = adminEmail, Email = adminEmail };
+//        var result = await userManager.CreateAsync(adminUser, "%0y@K[1I%59X\r\n"); // Set the password for the admin user
+//        if (result.Succeeded)
+//        {
+//            await userManager.AddToRoleAsync(adminUser, "adminRoleId");
+//        }
+//    }
+//}
+
 
 
 // Configure the HTTP request pipeline.
